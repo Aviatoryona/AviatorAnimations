77460d9ff492b335b2c3c65f597bf1afc3cc01c3
Aviatoryona <aviatoryona67@gmail.com>
2018-08-17 17:16:30 +0300

    Initial Commit

---
+++ aviatoranims/src/main/java/com/aviator/aviatoranims/AvAnimations.java
@@ -0,0 +1,68 @@
+package com.aviator.aviatoranims;
+
+import android.content.Context;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+
+@SuppressWarnings("ALL")
+public class AvAnimations {
+
+    public AvAnimations() {
+    }
+
+    public static Animation BottomFromTop(Context context) {
+        return AnimationUtils.loadAnimation(context, R.anim.bottom_from_top);
+    }
+
+    public static Animation Bounce(Context context) {
+        return AnimationUtils.loadAnimation(context, R.anim.bounce);
+    }
+
+    public static Animation FadeIn(Context context) {
+        return AnimationUtils.loadAnimation(context, R.anim.fade);
+    }
+
+    public static Animation FadeOut(Context context) {
+        return AnimationUtils.loadAnimation(context, R.anim.fade_out);
+    }
+
+    public static Animation LeftFromRight(Context context) {
+        return AnimationUtils.loadAnimation(context, R.anim.left_from_right);
+    }
+
+    public static Animation RightFromLeft(Context context) {
+        return AnimationUtils.loadAnimation(context, R.anim.right_from_left);
+    }
+
+    public static Animation SlideLeft(Context context) {
+        return AnimationUtils.loadAnimation(context, R.anim.slide_left);
+    }
+
+    public static Animation SlideRight(Context context) {
+        return AnimationUtils.loadAnimation(context, R.anim.slide_right);
+    }
+
+    public static Animation SlideUp(Context context) {
+        return AnimationUtils.loadAnimation(context, R.anim.slide_up);
+    }
+
+    public static Animation TopFromBottom(Context context) {
+        return AnimationUtils.loadAnimation(context, R.anim.top_from_bottom);
+    }
+
+    public static Animation UpFromBottom(Context context) {
+        return AnimationUtils.loadAnimation(context, R.anim.up_from_bottom);
+    }
+
+    public static Animation Welcome(Context context) {
+        return AnimationUtils.loadAnimation(context, R.anim.welcome);
+    }
+
+    public static Animation ZoomIn(Context context) {
+        return AnimationUtils.loadAnimation(context, R.anim.zoom_in);
+    }
+
+    public static Animation ZoomOut(Context context) {
+        return AnimationUtils.loadAnimation(context, R.anim.zoom_out);
+    }
+}
